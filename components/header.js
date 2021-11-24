231d833fe93628698feb9f44b5c1e912f83f30de
NTE ADMIN <mawayalebo@gmail.com>
2021-11-24 13:31:35 +0000

    top header

--- components/header.js
+++ components/header.js
@@ -1,20 +1,43 @@
+import { ChevronDownIcon, HeartIcon, ShoppingCartIcon } from '@heroicons/react/solid';
 import Image from 'next/image'
 const Header = () => {
     return ( 
         <div>
             {/* header top */}
-            <div className="flex items-center bg-white p-5">
-                <div className="flex items-center">
+            <div className="flex items-center bg-white p-5 mx-[45px]">
+                <div className="flex flex-1 items-center">
                     <div className="logo ">
                         <Image src="/images/logo.svg" alt="logo" width={180} height={37}/>
                     </div>
-                    <div>
-                        <div>
-                            <span>Help</span>
+                    <div className="flex px-2">
+                        <div className="p-2 border-r-2 border-solid border-gray-300">
+                            <span className="text-xs">Help</span>
                         </div>
-                        <div>
-                            <span>Sell on takealot</span>
+                        <div className="p-2">
+                            <span className=" text-xs">Sell on takealot</span>
                         </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 items-center justify-end">
+                    <div className="p-2 border-r-2 border-solid border-gray-300">
+                        <span className="text-xs">Login</span>
+                    </div>
+                    <div className="p-2 border-r-2 border-solid border-gray-300">
+                        <span className="text-xs">Register</span>
+                    </div>
+                    <div className="p-2 border-r-2 border-solid border-gray-300">
+                        <span className="text-xs">Order</span>
+                    </div>
+                    <div className="p-2 flex space-x-2">
+                        <span className="text-xs">My Account</span>
+                        <ChevronDownIcon className="h-4 text-gray-600" />
+                    </div>
+                    <div className="rounded-full bg-[#f4697a] p-1 hover:bg-red-600 ease-in transtition duration-500">
+                        <HeartIcon className="text-white h-5"/>
+                    </div>
+                    <div className="flex px-2 py-1 space-x-2 items-center bg-[#1c8644] rounded-full ml-2">
+                        <ShoppingCartIcon className="h-5 text-white"/>
+                        <span className="text-white">0</span>
                     </div>
                 </div>
                 <div>
