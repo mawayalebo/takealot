1d3b2f8481902c15ac05253b5f56040ac34c0ee2
NTE ADMIN <mawayalebo@gmail.com>
2021-12-01 08:09:46 +0000

    removing all the fetching

--- pages/index.js
+++ pages/index.js
@@ -28,18 +28,18 @@ import {  setDepartments, setThirdPartyData } from '../features/slices/uiData'
   )
 }
 export async function getStaticProps() {
-  const deptData = await fetch(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getDept`);
+ /* const deptData = await fetch(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getDept`);
   const departments = await deptData.json();
 
   const thirdPartyResponse = await fetch(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/thirdParty`);
   const thirdPartyData = await thirdPartyResponse.json();
   
-
+*/
   return {
       props: {
-          departments,
-          thirdPartyData
+          departments:[],
+          thirdPartyData:[]
       }
-  }
+  } 
 }
 export default Home
