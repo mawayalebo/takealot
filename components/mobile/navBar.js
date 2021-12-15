9722110339cd969e5c35b5ba82f19e8234a45b0f
NTE ADMIN <mawayalebo@gmail.com>
2021-12-15 13:29:04 +0200

    the navigator component

--- components/mobile/navBar.js
+++ components/mobile/navBar.js
@@ -1,13 +1,27 @@
 import Navigator from "./navigator";
-import { XIcon } from "@heroicons/react/solid";
+import { XIcon , ArrowRightIcon, ShoppingCartIcon, HeartIcon } from "@heroicons/react/solid";
 import { selectShowMobileNav, setShowMobileNav } from "../../features/slices/uiData";
 import { useSelector, useDispatch} from "react-redux";
+import { useState } from "react";
 
 const NavBar = () => {
+    const [ cartColor, setCartColor ] = useState("#0fcc5e");
     const dispatch = useDispatch();
     const showMobileNav = useSelector(selectShowMobileNav);
 
-    const data = {}
+    const set1 = [
+        {title:"Home", href:"/"},
+        {title:"Shop by Category" , href:"/category", actionIcon: ArrowRightIcon},
+        {title:"Deals"},
+        {title:"Orders"},
+        {title:"Account", href:"/account", actionIcon: ArrowRightIcon},
+        {title:"Help"}
+    ]
+
+    const set2 = [
+        {title:"Cart", icon:ShoppingCartIcon, icon_color:"" , href:"/cart", indicator: "0 Items"},
+        {title:"List", icon:HeartIcon, icon_color: "" , href:"/list", actionIcon: ArrowRightIcon},
+    ]
     return ( 
         <div className={`bg-[#0000008a] w-screen h-screen ${showMobileNav?"absolute":"hidden"} z-40 flex lg:hidden space-x-2`}>
                 <div className="flex flex-col bg-[#f0f0f0] w-[90vw] h-screen">
@@ -16,7 +30,8 @@ const NavBar = () => {
                             <img src="/images/logo.svg"  className="h-[23px] w-[111.03px] flex-1" />
                         </div>
                     </div>
-                    <Navigator data={data}/>
+                    <Navigator data={set1}/>
+                    <Navigator data={set2}/>
                 </div>
                 <div className="p-2">
                     <XIcon onClick={()=>{dispatch(setShowMobileNav(false))}} className="h-[23px] text-white hover:scale-110"/>
--- components/mobile/navigator.js
+++ components/mobile/navigator.js
@@ -1,7 +1,30 @@
+
 const Navigator = ({data}) => {
+    
     return ( 
-        <div>
-            
+        <div className="bg-white shadow-md flex flex-col mt-6">
+            {
+                data?.map((item, index) => {
+                    return (
+                        <div key={index} className="shadow-sm">
+                            <div className="flex p-4 space-x-4">
+                                {
+                                    item.icon?<item.icon className={` h-[24px] text-[${item.icon_color}]`} />:null
+                                }
+                                <div className="flex-1">
+                                    <span className="text-[#4d4d4f] text-xs">{item.title}</span>
+                                </div>
+                                {
+                                    item.indicator?<span className="text-xs text-[#4d4d4f]">{item.indicator}</span>:null
+                                }
+                                {
+                                    item.actionIcon?<item.actionIcon className="h-[14px] text-[#4d4d4f]" />:null
+                                }
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
      );
 }
